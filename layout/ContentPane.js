428220dc5e8c00c9aa20ec4c892ac84d36b31882
brgd <hohhots@gmail.com>
2014-04-08 00:40:55 +0800

    temp

---
+++ layout/ContentPane.js
@@ -0,0 +1,12 @@
+define([
+    "dojo/_base/declare", // declare
+	"../../dijit/layout/ContentPane",
+], function(declare, ContentPane){
+
+	// module:
+	//		mijit/layout/ContentPane
+
+	return declare("mijit.layout.mContentPane", ContentPane, {
+		
+	});
+});
--- tests/layout/module.js
+++ tests/layout/module.js
@@ -1,3 +1,3 @@
 define(["doh/main", "require"], function(doh, require){
-	doh.register("layout.mContentPane", require.toUrl("./mContentPane.html"), 999999);
+	doh.register("layout.ContentPane", require.toUrl("./ContentPane.html"), 999999);
 });
