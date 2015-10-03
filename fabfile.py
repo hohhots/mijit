1ee76f4c8b2f123f335ef8b6fbec0f72886e9cbf
sbrgd <hohhots@gmail.com>
2015-10-03 23:45:46 +0800

    add fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 # import fabrics API functions - self-explanatory once you see
 from fabric.api import *
-def pushpull():
+def git():
     local('git push') # runs the command on the local environment
     run('cd /home/pi/myProject/mijit; git pull') # runs the command on the remote environment
