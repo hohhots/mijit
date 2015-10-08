f5d7f638bc9b70ab1ac2511afd11732dd037f4d5
sbrgd <hohhots@gmail.com>
2015-10-08 21:55:54 +0800

    rename 2

--- scriptsForDev/fabfile.py
+++ scriptsForDev/fabfile.py
@@ -1,6 +1,8 @@
 # import fabrics API functions - self-explanatory once you see
 from fabric.api import env,local,run
+
 env.hosts = ['pi@192.168.81.1:22']
+
 def git():
     local('git push') # runs the command on the local environment
     run('cd /home/pi/myProject/mijit; git pull') # runs the command on the remote environment
