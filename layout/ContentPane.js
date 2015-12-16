70153e60cc5f9c5fc7618215f62a10e408e87890
brgd <hohhots@gmail.com>
2015-12-16 17:03:30 +0800

    start test contentpane

--- layout/ContentPane.js
+++ layout/ContentPane.js
@@ -1,18 +1,33 @@
 define([
 	"dojo/_base/declare",
 	"dojo/dom-construct",
-	"dijit/layout/ContentPane"
-], function( declare, domConstruct, dContentPane ){
+	"dijit/layout/ContentPane",
+	"dojo/dom-construct",
+	"dojo/dom-style"
+], function( declare, domConstruct, dContentPane, domConstruct, domStyle){
 
 	// module:
 	// mijit/layout/ContentPane
 
 	return declare("mijit.layout.ContentPane", dContentPane, {
+		
+		//A reference to wrapping div of ContentPane
+		parentDomNode: null,
+		
 		buildRendering: function(){
 			this.inherited(arguments);
 		},
+		
 		postCreate: function(){
-			// create the DOM for this widget
+			// create the DOM for wrapping div of this widget
+			this.parentDomNode = domConstruct.create("div");
+			domConstruct.place(this.parentDomNode,this.domNode,"before");
+			domConstruct.place(this.domNode,this.parentDomNode,"only");
+			
+			//rotate domNode
+			//domStyle.set(this.domNode,"transform-origin","left top");
+			domStyle.set(this.domNode,"transform","rotateZ(-90deg)");
+			domStyle.set(this.domNode,"transform","rotate(180deg)");
 			
 		}
 	});
